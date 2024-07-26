a8fdebb37b5ae48c05dd8b10e02ff5ee26fc5efd
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:19:25 -0500

    feat: Agregar programa para convertir grados Fahrenheit a grados Celsius en Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,7 @@
+#Programa que solicite al usuario los datos para llevar Grados Farenheit a Grados Celcius.
+
+farenheit = float(input("Introduce los grados Fahrenheit: "))
+
+celcius = (farenheit - 32) * 5.0/9.0
+
+print("Los grados Celcius son: ", + celcius)
