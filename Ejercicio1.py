5d9c9db44cf959d1df58762360752f851f5e288e
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:18:30 -0500

    Agregar mensaje inspirador de Albus Dumbledore en Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,5 @@
+# Escribir un programa que muestre por pantalla el siguiente mensaje:
+
 print('''La felicidad se puede encontrar hasta en los más oscuros
     momentos, si somos capaces de usar bien la luz.
     — Albus Dumbledore''')
