94c648f09f55b2c2c98cfc8e46b9896847d0f0e0
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:19:50 -0500

    chore: Agregar diferencias entre Python y JavaScript en Ejercicio10.py

---
+++ Ejercicio10.py
@@ -0,0 +1,10 @@
+#Escriba en comentarios de Python que diferencia tiene con JavaScript con Python, de 
+#acuerdo con lo visto en las sesiones de JavaScript.
+
+# Diferencias entre Python y JavaScript:
+
+# 1. Python es fácil de leer y escribir, casi como si fuera inglés, mientras que JavaScript puede ser un poco más complicado por sus símbolos y llaves {}.
+# 2. En Python, no tengo que preocuparme tanto por el tipo de datos, ya que es bastante flexible. En JavaScript, a veces, me encuentro con sorpresas porque no siempre se comporta como espero.
+# 3. Python me resulta genial para trabajar en proyectos de ciencia de datos y aprendizaje automático, mientras que JavaScript es mi opción cuando estoy desarrollando cosas para la web.
+# 4. En cuanto al trabajo en equipo, Python es excelente para la colaboración debido a su claridad, mientras que JavaScript me permite crear aplicaciones interactivas para el usuario final.
+# 5. Para mí, Python es como una herramienta versátil que puedo usar para casi cualquier cosa, pero si quiero hacer algo en la web, JavaScript es la mejor opción.
