86a05f9dd830331b8cb91140ab5bbf4389563a99
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:18:21 -0500

    Agregar ejemplos de tipos de datos y operaciones en Ejercicio0.py

---
+++ Ejercicio0.py
@@ -0,0 +1,63 @@
+#help('keywords')
+#entero=type(9)
+#print(entero)
+#flotante=type(9.0)
+#print(flotante)
+#cadena=type("9")
+#print(cadena)
+#booleano=type(True)
+#print(booleano)
+#complejo=type(9+9j)
+#print(complejo)
+#lista=type([1,2,3,4,5])
+#print(lista)
+#tupla=type((1,2,3,4,5))
+#print(tupla)
+#rango=type(range(5))
+#print(rango)
+#diccionario=type({"nombre":"Juan","edad":22})
+#print(diccionario)
+#conjunto=type({"manzana","pera","naranja"})
+#print(conjunto)
+#nulo=type(None)
+#print(nulo)
+
+#space_odyssey = 2001 
+#print(space_odyssey) 
+
+#print(type(5))
+
+#print(type(2.0))
+
+#result = 10 * 3.0
+#print(type(result))
+
+#num1 = 10
+#num2 = 3
+#result = num1 / num2
+#print(result)
+#print(type(result))
+#total_cars = 100
+#sold_cars = 20
+#total_cars = total_cars - sold_cars
+
+#num1 = 10
+#num2 = 3	
+#result = num1 // num2
+#result= int(result)
+#print (str(result))
+
+#msg = 'Necesitamos \ 'escapar\ ' las comillas simples'
+
+#num1 = input("Ingrese un numero: ")
+#num2 = input("Ingrese otro numero: ")
+#result = int(num1) + int(num2)
+#print('la suma es: ' + str(result))
+
+#print(result)
+
+#name = 'Elon Musk'
+#age = 49
+#fortune = 43_300
+
+#print(f'Me llamo {name}, tengo {age} años y una fortuna de {fortune} millones')
