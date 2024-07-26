9c24336b2230408a9dd726c8b22d5ead7f575464
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:19:00 -0500

    chore: Agregar programa para calcular el área de un Cuadrado en Ejercicio5.py

---
+++ Ejercicio5.py
@@ -0,0 +1,7 @@
+#Programa que solicite al usuario los datos para calcular el área de un Cuadrado (■), 
+#finalmente mostrar el resultado en pantalla.
+lado = float(input("Introduce la longitud del lado del cuadrado: "))
+
+areaC= lado * lado
+
+print("El área del cuadrado es: ", + areaC)
