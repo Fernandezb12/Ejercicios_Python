6326ef7bb8f1153037537b88f3e30f567bc17665
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:18:44 -0500

    feat: Agregar programa para saludar al usuario con su nombre completo en Ejercicio3.py

---
+++ Ejercicio3.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pregunte el nombre y apellido del usuario. Luego de ser 
+#introducidos los datos muestre por pantalla la cadena ¡Hola {nombre} {apellido}, gusto en 
+#conocerte!, donde:{nombre} y {apellido} son los valores introducidos.
+
+nombre = "Daniel"
+apellido = "Fernandez"
+print("Hola, "+ nombre + " " + apellido+" , Gusto conocerte")
