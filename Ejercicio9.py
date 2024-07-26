027016953c772afb8609a4d9b6d1b2cae34c5ae5
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:19:33 -0500

    feat: Agregar programa para calcular el precio semanal y mensual de un freelancer en Ejercicio9.py

---
+++ Ejercicio9.py
@@ -0,0 +1,13 @@
+#Un Freelancer desea saber cuánto puede cobrar por su trabajo semanal y mensualmente, 
+#para ello solo necesita establecer el precio de su trabajo por hora. Se estiman 40 horas de 
+#trabajo a la semana
+
+precio_hora = float(input("Ingrese el precio por hora: "))
+precio_semanal = 40 * precio_hora
+precio_mensual = precio_semanal * 4
+
+print("El precio semanal es: ", + precio_semanal)
+print("El precio mensual es: ", + precio_mensual)
+
+
+
