d03dcd9e1a227a2aed553002cf16b02a1bb700a6
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-26 10:19:06 -0500

    feat: Agregar programa para calcular el área de un Triángulo en Ejercicio6.py

---
+++ Ejercicio6.py
@@ -0,0 +1,9 @@
+#Programa que solicite al usuario los datos para calcular el área de un Triángulo (▲), 
+#finalmente mostrar el resultado en pantalla.
+
+base = float(input("Introduce la base del triángulo: "))
+altura = float(input("Introduce la altura del triángulo: "))
+
+areaT = (base * altura) / 2
+
+print("El área del triángulo es: ", + areaT)
