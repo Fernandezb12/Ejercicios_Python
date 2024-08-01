a53d69899268fc1ecb20050018e16f00dc4df9ae
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-31 22:05:05 -0500

    Agregar archivo listas.py con ejemplos de manipulación de listas

---
+++ listas.py
@@ -0,0 +1,20 @@
+'''shopping = ['Agua', 'Huevos', 'Aceite', 'Sal', 'Limón']
+
+shopping[0:3]
+
+shopping[:3]
+
+shopping[2:4]
+
+shopping[-1:-4:-1]
+
+# Equivale a invertir la lista
+shopping[::-1]'''
+
+'''even_numbers = []
+
+for i in range(20):
+    if i % 2 == 1:
+        even_numbers.append(i)
+
+even_numbers'''
