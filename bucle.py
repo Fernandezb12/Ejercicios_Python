37792db1418fd0baaae15405a88a0a9b09d8242e
DanielFernandez <daniele_fernandezb@soy.sena.edu.co>
2024-07-31 22:05:10 -0500

    feat: Agregar ejemplos de bucles while y for en bucle.py

---
+++ bucle.py
@@ -0,0 +1,14 @@
+# while
+'''want_greet = 'S'  # importante dar un valor inicial
+
+while want_greet == 'S':
+    print('Hola qué tal!')
+    want_greet = input('¿Quiere otro saludo? [S/N] ')
+print('Que tenga un buen día')'''
+
+# for
+
+''' word = 'Python'
+
+for letter in word:
+    print(letter)'''
